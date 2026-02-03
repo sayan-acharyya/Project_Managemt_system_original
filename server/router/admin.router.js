2cff37658a88823769d045bb74aff9fa0995510e
sayan-acharyya <acharyyas735@gmail.com>
2026-02-04 02:16:42 +0530

    ...

--- server/router/admin.router.js
+++ server/router/admin.router.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.post("/create-student",isAuthenticated,isAuthorized("Admin"),createStudent);
 router.put("/update-student/:id",isAuthenticated,isAuthorized("Admin"),updateStudent);
 router.delete("/delete-student/:id",isAuthenticated,isAuthorized("Admin"),deleteStudent);
-
+   
 export default router;
 
