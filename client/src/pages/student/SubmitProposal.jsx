f2a4aecd8118de6bac75b355a7649f1cd349af5e
sayan-acharyya <acharyyas735@gmail.com>
2026-02-13 00:02:34 +0530

    submit proposal done

--- client/src/pages/student/SubmitProposal.jsx
+++ client/src/pages/student/SubmitProposal.jsx
@@ -1,9 +1,40 @@
- import React from 'react'
- 
- const SubmitProposal = () => {
-   return (
-     <div>SubmitProposal</div>
-   )
- }
- 
- export default SubmitProposal
+import React from 'react'
+import { submitProjectProposal } from '../../store/slices/studentSlice'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux';
+
+
+const SubmitProposal = () => {
+
+  const [formData, setFormData] = useState({
+    title: "",
+    description: ""
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      dispatch(submitProjectProposal(formData));
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+    }
+
+  }
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default SubmitProposal
--- client/src/store/slices/studentSlice.js
+++ client/src/store/slices/studentSlice.js
@@ -1,1 +1,36 @@
- 
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../lib/axios";
+import { toast } from "react-toastify";
+
+
+export const submitProjectProposal = createAsyncThunk(
+    "submitProjectProposal",
+    async (data, thunkAPI) => {
+        try {
+            const res = await axiosInstance.post("/student/project-proposal", data);
+            toast.success("Project proposal submitted successfully");
+            return res.data.data?.project || res.data.data || res.data;
+        } catch (error) {
+
+            toast.success("Previous project not rejects.can't submit new prposal" || error.response.data.message)
+        }
+        return thunkAPI.rejectWithValue(error.response.data.message)
+    }
+)
+const studentSlice = createSlice({
+    name: "student",
+    initialState: {
+        project: null,
+        files: [],
+        supervisors: [],
+        dashboardStats: [],
+        supervisor: null,
+        deadlines: [],
+        feedback: [],
+        status: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => { },
+});
+
+export default studentSlice.reducer;
