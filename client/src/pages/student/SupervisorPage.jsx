83174414c3e266488792b3a1aaabb8884d989c51
sayan-acharyya <acharyyas735@gmail.com>
2026-02-14 00:39:25 +0530

    on going

--- client/src/pages/student/SupervisorPage.jsx
+++ client/src/pages/student/SupervisorPage.jsx
@@ -1,9 +1,57 @@
- import React from 'react'
- 
- const SupervisorPage = () => {
-   return (
-     <div>SupervisorPage</div>
-   )
- }
- 
- export default SupervisorPage 
+import React, { useEffect, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import {
+  fetchAllSupervisor,
+  fetchProject,
+  getSupervisor,
+  requestSupervisor
+} from "../../store/slices/studentSlice"
+
+
+const SupervisorPage = () => {
+  const dispatch = useDispatch();
+  const { authUser } = useSelector(state => state.auth);
+  const { project, supervisors, supervisor } = useSelector(state => state.student);
+
+  const [showRequestModel, setShowRequestModel] = useState(false);
+  const [requestMessage, setRequestMessage] = useState("");
+  const [selectedSupervisor, setSelectedSupervisor] = useState(null);
+
+  useEffect(() => {
+    dispatch(fetchProject());
+    dispatch(getSupervisor());
+    dispatch(fetchAllSupervisor())
+  }, [dispatch])
+
+  const hashSupervisor = useMemo(
+    () => !!(supervisor && supervisor._id),
+    [supervisor]
+  )
+
+  const formatDeadline = (dateStr) => {
+    if (!dateStr) return "-";
+
+    const date = new Date(dateStr);
+
+    if (isNaN(date.getTime())) return "-";
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // Month starts from 0
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  };
+
+  const handleOpenRequest = (supervisor) => {
+    setSelectedSupervisor(supervisor);
+    setShowRequestModel(true);
+  }
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default SupervisorPage 
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import popupReducer from "./slices/popupSlice";
 import adminReducer from "./slices/adminSlice";
-
+import studentReducer from "./slices/studentSlice"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         popup: popupReducer,
         admin: adminReducer,
+        student: studentReducer,
+        // deadline: deadlineReducer,
+        // notification: noticationReducer,
+        // project: projectReducer,
+        // request: requestReducer,
+        // teacher: teacherReducer,
     },
 });
