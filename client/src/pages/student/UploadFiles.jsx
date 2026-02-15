09aca189eb1ff38b8f179ed99250e333e6b2acae
sayan-acharyya <acharyyas735@gmail.com>
2026-02-16 01:07:04 +0530

    ui design is going on

--- client/src/pages/student/UploadFiles.jsx
+++ client/src/pages/student/UploadFiles.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { File } from "lucide-react"
+import { Archive, File, FileCodeCorner, FileText, Presentation } from "lucide-react"
 import { useEffect } from 'react';
 import { useRef } from 'react';
 import { useState } from 'react';
@@ -71,7 +71,81 @@ const UploadFiles = () => {
 
   return (
     <>
+      <div className='space-y-6'>
+        <div className='card'>
+          <div className='card-header'>
+            <h1 className='card-title'>Upload Project Files</h1>
+            <p className='card-subtitle'>
+              upload your project decuments including
+              reports, presentations and code files.
+            </p>
+          </div>
 
+          {/* UPLOAD SECTION */}
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-3'>
+            <div className='border-2 border-dashed mt-5 border-slate-300 rounded-lg 
+            p-6 text-center hover:border-blue-400 transition-colors '>
+              <div className='mb-4'>
+                <FileText className='w-12 h-12 text-slate-400 mx-auto ' />
+              </div>
+              <h3 className='text-lg font-medium text-slate-800 mb-2'>Report</h3>
+              <p className='text-sm text-slate-600 mb-4'>Upload your project report(PDF,DOC)</p>
+              <label className='btn-outline cursor-pointer'>
+                Choose File
+                <input
+                  type="file"
+                  ref={reportRef}
+                  className='hidden' accept='.pdf,.doc,.docx '
+                  onChange={handleFilePick}
+                  multiple
+                />
+              </label>
+            </div>
+
+
+            <div className='border-2 border-dashed mt-5 border-slate-300 rounded-lg 
+            p-6 text-center hover:border-blue-400 transition-colors '>
+              <div className='mb-4'>
+                <Presentation className='w-12 h-12 text-slate-400 mx-auto ' />
+              </div>
+              <h3 className='text-lg font-medium text-slate-800 mb-2'>Presentation</h3>
+              <p className='text-sm text-slate-600 mb-4'>Upload your project Presentation(PPT,PPTX,PDF)</p>
+              <label className='btn-outline cursor-pointer'>
+                Choose File
+                <input
+                  type="file"
+                  ref={presRef}
+                  className='hidden' accept='.pdf,.ppt,.pptx '
+                  onChange={handleFilePick}
+                  multiple
+                />
+              </label>
+            </div>
+
+            <div className='border-2 border-dashed mt-5 border-slate-300 rounded-lg 
+            p-6 text-center hover:border-blue-400 transition-colors '>
+              <div className='mb-4'>
+                <FileCodeCorner className='w-12 h-12 text-slate-400 mx-auto ' />
+              </div>
+              <h3 className='text-lg font-medium text-slate-800 mb-2'>Code Files</h3>
+              <p className='text-sm text-slate-600 mb-4'>Upload your project Source Code(ZIP,RAR)</p>
+              <label className='btn-outline cursor-pointer'>
+                Choose File
+                <input
+                  type="file"
+                  ref={codeRef}
+                  className='hidden' accept='.zip,.rar,.tar,.gz'
+                  onChange={handleFilePick}
+                  multiple
+                />
+              </label>
+            </div>
+
+          </div>
+
+          
+        </div>
+      </div>
     </>
   )
 }
